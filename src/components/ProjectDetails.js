02314e1ae94f84f5614c6ce9d0ca713faf2d00a2
dogayolcu <83917987+dogayolcu@users.noreply.github.com>
2023-12-28 01:05:42 +0300

    filtreleme

--- src/components/ProjectDetails.js
+++ src/components/ProjectDetails.js
@@ -6,31 +6,22 @@ const ProjectDetails = () => {
   const [projects, setProjects] = useState([]);
   const [selectedProjectId, setSelectedProjectId] = useState('');
   const [members, setMembers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   const { user } = useContext(UserContext);
 
   useEffect(() => {
-
     if (user && user.id) {
       axios.get(`http://localhost:8080/api/v1/projects/user/${user.id}/projects`)
-        .then(response => {
-          setProjects(response.data);
-        })
-        .catch(error => {
-          console.error("Error loading user projects:", error);
-        });
+        .then(response => setProjects(response.data))
+        .catch(error => console.error("Error loading user projects:", error));
     }
   }, [user]);
 
   useEffect(() => {
-
     if (selectedProjectId) {
       axios.get(`http://localhost:8080/api/v1/projects/${selectedProjectId}/members`)
-        .then(response => {
-          setMembers(response.data);
-        })
-        .catch(error => {
-          console.error("Error loading project members:", error);
-        });
+        .then(response => setMembers(response.data))
+        .catch(error => console.error("Error loading project members:", error));
     }
   }, [selectedProjectId]);
 
@@ -38,22 +29,35 @@ const ProjectDetails = () => {
     setSelectedProjectId(e.target.value);
   };
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredMembers = members.filter(member =>
+    member.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="container">
       <div className="project-details">
         <h2>Selected Project</h2>
         <select id="project" name="project" value={selectedProjectId} onChange={handleProjectChange}>
           <option value="">Select a project</option>
-          {projects.map((project) => (
-            <option key={project.id} value={project.id}>
-              {project.name}
-            </option>
+          {projects.map(project => (
+            <option key={project.id} value={project.id}>{project.name}</option>
           ))}
         </select>
 
+        <input 
+          type="text" 
+          placeholder="Search by member name..." 
+          value={searchTerm} 
+          onChange={handleSearchChange} 
+        />
+
         <h2>Project Members</h2>
         <ul>
-          {members.map((member) => (
+          {filteredMembers.map(member => (
             <li key={member.id}>{member.name} {member.surname}</li>
           ))}
         </ul>
